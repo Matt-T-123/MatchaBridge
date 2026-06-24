0f7e95a234c422f42f507f6818e5ff28f6720903
Matt.T <49764813+Matt-T-123@users.noreply.github.com>
2026-06-24 13:11:22 +0100

    Create LIBRARY.lua

---
+++ LIBRARY.lua
@@ -0,0 +1,23 @@
+local LIBRARY = {
+  [0] = { --GLOBAL
+    {
+			name = "InfYeld",
+			description = "Inf Yield gay edition",
+			code = "https://raw.githubusercontent.com/Sayf199/Matcha-External-VM-InfYeld/main/InfYeld.lua",
+		},
+  },
+	[155615604] = { --PRISON LIFE
+		{
+			name = "Check it",
+			description = "Prison life shit",
+			code = "https://raw.githubusercontent.com/hitechboi/hhhitechboi/refs/heads/main/uni.lua",
+		},
+	},
+  [606849621] = { -- JAILBREAK
+    {
+      name = "",
+      description = "",
+      code = "",
+    },
+  },
+}
